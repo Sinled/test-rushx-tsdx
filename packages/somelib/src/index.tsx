c37360a02efae7569169b16cda8640392f1b0988
Dmitriy Dovgan <d_dovgan@wargaming.net>
2021-01-11 18:38:01 +0200

    master. Fix lint

--- packages/somelib/src/index.tsx
+++ packages/somelib/src/index.tsx
@@ -1,40 +1,51 @@
 import React from 'react';
 import { IColumn, SelectionMode } from 'office-ui-fabric-react/lib/DetailsList';
-import { ShimmeredDetailsList, IShimmeredDetailsListProps } from 'office-ui-fabric-react/lib/ShimmeredDetailsList';
+import {
+  ShimmeredDetailsList,
+  IShimmeredDetailsListProps,
+} from 'office-ui-fabric-react/lib/ShimmeredDetailsList';
+import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
 
 const shimmeredDetailsListProps: IShimmeredDetailsListProps['listProps'] = {
-	renderedWindowsAhead: 0,
-	renderedWindowsBehind: 0,
+  renderedWindowsAhead: 0,
+  renderedWindowsBehind: 0,
 };
 
-export const ShimmerApplicationExample: React.FunctionComponent = () => {
-	const columns: Array<IColumn> = [
-		{
-			key: 'name',
-			name: 'Name',
-			minWidth: 0,
-		},
-		{
-			key: 'description',
-			name: 'Description',
-			minWidth: 0,
-		},
-	];
+const classNames = mergeStyleSets({
+  root: {
+    color: 'red',
+  },
+});
 
-	return (
-		<>
-			<div>
-				<ShimmeredDetailsList
-					setKey="items"
-					items={[]}
-					columns={columns}
-					selectionMode={SelectionMode.none}
-					enableShimmer={true}
-					ariaLabelForShimmer="Content is being fetched"
-					ariaLabelForGrid="Item details"
-					listProps={shimmeredDetailsListProps}
-				/>
-			</div>
-		</>
-	);
+export const ShimmerApplicationExample: React.FunctionComponent = () => {
+  const columns: Array<IColumn> = [
+    {
+      key: 'name',
+      name: 'Name',
+      minWidth: 0,
+    },
+    {
+      key: 'description',
+      name: 'Description',
+      minWidth: 0,
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        <ShimmeredDetailsList
+          className={classNames.root}
+          setKey="items"
+          items={[]}
+          columns={columns}
+          selectionMode={SelectionMode.none}
+          enableShimmer={true}
+          ariaLabelForShimmer="Content is being fetched"
+          ariaLabelForGrid="Item details"
+          listProps={shimmeredDetailsListProps}
+        />
+      </div>
+    </>
+  );
 };
